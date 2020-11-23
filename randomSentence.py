14ffaa205fbc9259a28719912676618cea640e4b
Thomas Manuel <tman610@aucklanduni.ac.nz>
2020-11-23 18:59:47 +1300

    removed web scraping, added simple random sentences

--- randomSentence.py
+++ randomSentence.py
@@ -1,40 +1,25 @@
-from bs4 import BeautifulSoup
 from random import randrange
-#This file generates a random sentence to be used for typing practice
 
-import urllib.request
+numNouns = 1525
+numVerbs = 1011
+numAdverbs = 255
+numAdjectives = 527
 
-linkList = []
-returnText = ""
+def getRandomWord(fileName, n):
+    randNoun = randrange(n)
+    with open(fileName) as fp:
+        for i, line in enumerate(fp):
+            if i == randNoun:
+                return line.strip()
 
+def getRandomSentence():
+    sentence = ""
+    sentence += getRandomWord("adjectives.txt", numAdjectives).title() + " "
+    sentence += getRandomWord("nouns.txt", numNouns) + " "
+    sentence += getRandomWord("verbs.txt", numVerbs) + "s "
+    sentence += getRandomWord("adjectives.txt", numAdjectives) + " "
+    sentence += getRandomWord("nouns.txt", numNouns) + " "
+    sentence += getRandomWord("adverbs.txt", numAdverbs) + "."
+    print(sentence)
 
-fp = urllib.request.urlopen("http://www.magickeys.com/books/")
-mybytes = fp.read()
-mystr = mybytes.decode("utf8")
-fp.close()
-soup = BeautifulSoup(mystr, features="html5lib")#make soup that is parse-able by bs
-for link in soup.find_all('a'):
-    candidatePage = link.get('href')
-    mustContain = "http://www.magickeys.com/books/"
-    if (candidatePage):
-        if mustContain in candidatePage:
-            if (candidatePage not in linkList):
-                linkList.append(candidatePage)
-
-randNum = randrange(len(linkList))
-
-fp = urllib.request.urlopen(linkList[randNum])
-mybytes = fp.read()
-mystr = mybytes.decode("utf8")
-fp.close()
-soup = BeautifulSoup(mystr, "html.parser")#make soup that is parse-able by bs
-page = soup.find_all("p")
-for tag in page:
-    text = tag.text.split(".")
-    for i in text:
-        newText = i.strip() + ". "
-        if len(newText) > 2:
-            returnText = returnText + newText
-returnText = returnText.strip()
-print(returnText)
-print(len(returnText))
+getRandomSentence()
